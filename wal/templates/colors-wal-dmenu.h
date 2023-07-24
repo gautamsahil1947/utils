2ae5ed3751e300f32ad4a3f32928292aabf0907d
sahil <gautamsahil1947@gmail.com>
2023-07-24 23:34:53 +0530

    added dwmblocks

---
+++ wal/templates/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {{
+	/*     fg         bg       */
+	[SchemeNorm] = {{ "{color4}", "{color0}" }},
+	[SchemeSel] = {{ "{color0}", "{color4}" }},
+	[SchemeOut] = {{ "{color15}", "{color14}" }},
+}};
